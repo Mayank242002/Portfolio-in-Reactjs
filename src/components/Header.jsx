0fdb012891446d57a3cd72984265de7fede1d85c
Mayank242002 <msngi24july@gmail.com>
2022-07-28 09:38:13 +0530

    Changing simple portfolio to its equivalent Reactjs version

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header-styles.css"
+
+function Header()
+{
+    return (<div id="header" >
+        <a className="header-left" href="">Mayank</a>
+        <a className="header-right" href="#About">About</a>
+        <a className="header-right" href="#Skills">Skills</a>
+        <a className="header-right" href="#Project">Project</a>
+        <a className="header-right" href="#Contact">Contact</a>
+    </div>);
+}
+
+export default Header;
---
+++ src/components/Skills.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Skills()
+{
+    return (<div></div>);
+}
+
+export default Skills;
