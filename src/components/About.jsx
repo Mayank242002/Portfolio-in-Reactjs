72a6e00a693f82d6910e73ae0ed12aebf46d0f0c
Mayank242002 <msngi24july@gmail.com>
2022-08-05 10:11:31 +0530

    images and About Component added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,36 @@
 import React from "react";
+import "./About-styles.css";
 
 function Header()
 {
-    return (<div></div>);
+    return (
+        <div id="about">
+        <div class="about-information">
+            <h1 style={{fontSize:"40px"}}>Hey _______</h1>
+            <h2 style={{fontSize:"60px"}}>I AM MAYANK</h2>
+            <h3 style={{fontSize:"30px"}}>Junior Web Developer</h3>
+        </div>
+        <img src={require("../images/profile.jpg")} alt="profile-image1"/>
+        <img src={require("../images/profile2.jpg")} alt="profile-image2"/>
+        <div class="about-information2">
+            <h1>LET ME </h1>
+            <h1>INTRODUCE</h1>
+            <h1>MYSELF</h1>
+            <br/>
+            <div style={{width:"400px"}}>
+                <p style={{wordSpacing: "3px"}}>Hey Everyone,Mayank Singh Negi here.I'm currently pursuing BTECH from
+                    Graphic Era deemed To be
+                    University,Dehradun</p>
+                <br/>
+                <p style={{wordSpacing: "3px"}}>I'm a  Developer.I also have Basic knowledge of Data Structures and Algorithms, Google cloud
+                    platform,Azure,flutter</p>
+                <br/>
+                <button class="button"><a href="https://drive.google.com/file/d/1bTJZn_rMocTWGMtGbUyJkaKfSB1bODaf/view?usp=sharing" download={"Mayank Resume"}>Get Resume</a></button>
+            </div>
+
+        </div>
+    </div>
+    );
 }
 
 export default Header;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import "./Header-styles.css"
 function Header()
 {
     return (<div id="header" >
-        <a className="header-left" href="">Mayank</a>
+        <a className="header-left" href="#Name">Mayank</a>
         <a className="header-right" href="#About">About</a>
         <a className="header-right" href="#Skills">Skills</a>
         <a className="header-right" href="#Project">Project</a>
